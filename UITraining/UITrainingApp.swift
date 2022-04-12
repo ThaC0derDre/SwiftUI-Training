f41de7ef9b028eed8992fe47ec803f15ec0f01f3
AndresGutierrez <c0dingw.dre@gmail.com>
2022-04-12 12:04:08 -0700

    Initial Commit

---
+++ UITraining/UITrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITrainingApp.swift
+//  UITraining
+//
+//  Created by Andres Gutierrez on 4/12/22.
+//
+
+import SwiftUI
+
+@main
+struct UITrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
