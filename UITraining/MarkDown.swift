15e37d15784c95e9c4be889c824faa828ca71710
AndresGutierrez <c0dingw.dre@gmail.com>
2022-05-24 19:03:17 -0700

    More Markdown Stuff

--- UITraining/MarkDown.swift
+++ UITraining/MarkDown.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct MarkDown: View {
     var body: some View {
-        VStack {
+        VStack(spacing: 20) {
             //Add bold with **
             Text("**Login**")
             //Italics with *
             Text("*Make sure to provide your email address*")
+            //Together now!
+            Text("***Bold and Italic!***")
             //Create links with [] followed by (url)!
             Text("Click [Here](https://swiftlyrush.com) to view terms and conditions")
+            //Together now!
+            Text("Click on ***[THIS]***(https://google.com) to go to google")
+            //Strike through with ~~, one or two?
+            Text("~~A strikethrough example~~")
+            Text("~A strikethrough example~")
+            Text("`Monospaced works too`")
         }
     }
 }
